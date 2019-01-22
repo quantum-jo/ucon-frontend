1b4d93d81ab11aadf80bdba78e3133769e1f07cc
Rohith Balaji <rohithbalaji123@gmail.com>
2019-01-22 09:41:32 +0530

    Add login page after integration

---
+++ JS/login.js
@@ -0,0 +1,52 @@
+$(document).ready(function() {
+  $("#loginForm").submit(function(event) {
+    event.preventDefault();
+    var username = $("#login-username").val();
+    var password = $("#login-password").val();
+    var formData = {
+      username: username,
+      password: password,
+    };
+    $.ajax({
+      method: "POST",
+      url: "http://localhost:3000/login",
+      data: formData,
+      success: (result) => {
+        if(result.isValid) {
+          localStorage.setItem("jwtToken", result.jwtToken);
+          window.location.href = "./dashboard.html";
+        } else {
+          $("#loginMessage").html(result.message);
+        }
+      },
+      error: (error) => {
+        $("#loginMessage").html(error.responseJSON.message);
+      }
+    });
+  });
+
+  $("#registerForm").submit(function(event) {
+    event.preventDefault();
+    var username = $("#register-username").val();
+    var password = $("#register-password").val();
+    var name = $("#register-name").val();
+    var bio = $("#register-bio").val();
+    var formData = {
+      username: username,
+      password: password,
+      name: name,
+      bio: bio,
+    };
+    $.ajax({
+      method: "POST",
+      url: "http://localhost:3000/register", 
+      data: formData,
+      success: (result) => {
+        $("#registerMessage").html(result.message);
+      },
+      error: (error) => {
+        $("#registerMessage").html(error.responseJSON.message);
+      }
+    });
+  });
+});
